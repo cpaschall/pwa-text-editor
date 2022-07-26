7ff746cdff6a385551dc394f2a81abeaacb32908
cpaschall <cpaschall216@gmail.com>
2022-07-25 22:15:51 -0400

    adding to database.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -13,7 +13,7 @@ const pageCache = new CacheFirst({
     new CacheableResponsePlugin({
       statuses: [0, 200],
     }),
-    new ExpirationPlugin({e
+    new ExpirationPlugin({
       maxAgeSeconds: 30 * 24 * 60 * 60,
     }),
   ],
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -27,7 +27,22 @@ module.exports = () => {
         swDest: 'src-sw.js'
       }),
       new WebpackPwaManifest({
-
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Text editor...in your browser!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
       })
       
     ],
